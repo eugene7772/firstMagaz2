441f53d19f2a3b436dfca5fa1db5c8e159a60176
Eugene Fom <Eugene_fom@mail.ru>
2022-04-17 11:41:14 +0300

    Main page done

--- src/main/java/com/magaz2/firstMagaz2/controllers/MainControler.java
+++ src/main/java/com/magaz2/firstMagaz2/controllers/MainControler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 @Controller
@@ -21,10 +22,20 @@ public class MainControler {
     @GetMapping("/")
     public String home(Model model) {
         Iterable<ProductType> types = productTypeRepository.findAll();
+
+        List<Product> products = productRepository.findAll();
+        products.subList(3,products.size()).clear();
+
+        List<Product> products2 = productRepository.findAll();
+        products2.subList(6,products2.size()).clear();
+        products2.subList(0,3).clear();
+
         Map<ProductType, List<Product>> map = new HashMap<>();
         types.forEach(type->map.put(type, productRepository.findByProductType(type)));
         model.addAttribute("map",map);
         model.addAttribute("types",types);
+        model.addAttribute("products", products);
+        model.addAttribute("products2", products2);
         return "home";
     }
 
--- src/main/java/com/magaz2/firstMagaz2/controllers/ProductRepository.java
+++ src/main/java/com/magaz2/firstMagaz2/controllers/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.magaz2.firstMagaz2.controllers;
 
-import com.magaz2.firstMagaz2.controllers.Product;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,6 @@ import java.util.List;
 @Qualifier("productRepository")
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    public List<Product> findByProductType(ProductType productType);
+     List<Product> findByProductType(ProductType productType);
+     List<Product> findAll();
 }
